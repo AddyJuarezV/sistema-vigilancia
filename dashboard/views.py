af60e18ee8f206125fd884ddd4a37e4ead73f7e0
D4VIDZN <j.juarezv@upam.edu.mx>
2026-09-11 11:03:40 -0600

    Agregar roles de usuario y cierre de sesion

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,4 +4,14 @@ from django.shortcuts import render
 
 @login_required
 def inicio(request):
-    return render(request, "dashboard/inicio.html")
+    es_admin = request.user.groups.filter(name="Administrador").exists()
+    es_guardia = request.user.groups.filter(name="Guardia").exists()
+
+    return render(
+        request,
+        "dashboard/inicio.html",
+        {
+            "es_admin": es_admin,
+            "es_guardia": es_guardia,
+        }
+    )
